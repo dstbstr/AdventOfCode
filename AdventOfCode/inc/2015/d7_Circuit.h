52c4fdb822962f1fc903105423ef021afd9cda4f
Dustin Randall <durandal@microsoft.com>
2024-10-10 17:01:07 -0700

    Copied solutions over

---
+++ AdventOfCode/inc/2015/d7_Circuit.h
@@ -0,0 +1,3 @@
+#pragma once
+#include "Common.h"
+DECLARE_SOLUTION(2015, 7);
---
+++ AdventOfCode/inc/BoilerPlate.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Common.h"
+DECLARE_SOLUTION(2015, 1, R"RawInput_(InputGoesHere)RawInput_");
+
+//Cpp File
+SOLUTION(2015, 1) {
+    PART_ONE() {
+        return "";
+    }
+    PART_TWO() {
+        return "";
+    }
+
+    TESTS() {
+        return true;
+    }
+}
